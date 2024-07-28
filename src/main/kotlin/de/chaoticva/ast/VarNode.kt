58860ed735fbebdb5259c949950ba2fe830ae433
chaoticva <chaosbray@gmail.com>
2024-07-28 04:29:12 +0200

    quickfix for variable declaration const in error message

--- src/main/kotlin/de/chaoticva/ast/VarNode.kt
+++ src/main/kotlin/de/chaoticva/ast/VarNode.kt
@@ -1,7 +1,7 @@
 package de.chaoticva.ast
 
-class VarNode(val name: String, val type: String, val value: ASTNode, val const: Boolean? = false, override val lineStart: Int, override val lineEnd: Int, line: Int): ASTNode(lineStart, lineEnd, line) {
+class VarNode(val name: String, val type: String, val value: ASTNode, val const: Boolean = false, override val lineStart: Int, override val lineEnd: Int, line: Int): ASTNode(lineStart, lineEnd, line) {
     override fun toString(): String {
-        return "${if (const != null) "const" else ""} var $type $name = $value"
+        return "${if (const) "const " else ""}var $type $name = $value"
     }
 }
